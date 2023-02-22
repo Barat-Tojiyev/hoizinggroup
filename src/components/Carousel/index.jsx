91007a7287c3c30b0d6ec0a4b0de6f053ab0fc82
Barat-Tojiyev <barattojiyev@gmail.com>
2023-02-22 14:01:31 +0500

    Carousel end

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,6 +1,6 @@
 import { Carousel } from 'antd'
 import React, { useRef } from 'react'
-import { Arrow, Blur, Container,Image,Content } from './style'
+import { Arrow, Blur, Container,Image,Content, ArrowLeft } from './style'
 import Home1 from '../../assets/img/house1.jpg'
 import Home2 from '../../assets/img/image2.jpg'
 
@@ -8,9 +8,7 @@ import Home2 from '../../assets/img/image2.jpg'
 
 
 export const CarouselCard = () => {
-    const onChange = (currentSlide) => {
-        console.log(currentSlide);
-      };
+   
 
       const corusel=useRef()
       const onMove=({target:{dataset:{name}}})=>{
@@ -19,7 +17,7 @@ export const CarouselCard = () => {
       }
   return (
     <Container>
-        <Carousel ref={corusel}  afterChange={onChange}>
+        <Carousel ref={corusel}  >
       <Image src={Home1} alt="#" />
       <Image src={Home2} alt="#" />
     </Carousel>
@@ -29,9 +27,9 @@ export const CarouselCard = () => {
       Skyper Pool Partment
       </Content.Title>
     <Content.SubTitle>112 Glenwood Ave Hyde Park, Boston, MA</Content.SubTitle>  
-    <Content.Title>$5,250 / mo</Content.Title>   
+    <Content.Price>$5,250 / mo</Content.Price>   
     </Content>
-        <Arrow onClick={onMove} data-name='left' left/>
+        <ArrowLeft onClick={onMove} data-name='left' />
         <Arrow onClick={onMove} data-name='right' />
     </Container>
   )
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -19,6 +19,19 @@ left:${({left})=> left && '20px'};
 right:${({left})=> !left && '20px'};
 transform: ${({left})=> left ? 'rotate(0deg)':'rotate(180deg)'};
 `
+const ArrowLeft=styled(arrow)`
+width: 45px;
+height: 45px;
+padding: 15px;
+border-radius: 50%;
+background: rgba(255,255,255,0.4);
+cursor: pointer;
+position: absolute;
+top: 50%;
+left:${({right})=> !right && '20px'};
+/* right:${({right})=> right && '20px'}; */
+/* transform: ${({left})=> left ? 'rotate(0deg)':'rotate(180deg)'}; */
+`
 const Blur =styled.div`
 position: absolute;
 top:0;
@@ -53,8 +66,15 @@ font-weight: 400;
 font-size: 16px;
 line-height: 24px;
 `
+Content.Price=styled.h1`
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 28px;
+line-height: 36px;
+`
 
 const Image=styled.img`
 width:100%;
 height:571px;`
-export{Container,Arrow,Image,Blur,Content}
+export{Container, Arrow, Image, Blur, Content,ArrowLeft}
